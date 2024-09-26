a1f58dcb23660bb16be6b8b442b8d7fd9e66e38a
CJLynch01 <cjlynch@byui.edu>
2024-09-26 13:08:08 -0600

    Server up and working

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
+const port = process.env.PORT || 8080
 
-app.listen(8080, () => {
-    console.log('Server is running on port 8080 ');
+app.listen(port, () => {
+    console.log(`Server is running on port ${port} `);
 });
